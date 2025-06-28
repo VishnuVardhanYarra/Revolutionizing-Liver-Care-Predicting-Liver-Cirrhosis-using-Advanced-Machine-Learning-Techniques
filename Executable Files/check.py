8b0dd49be916aa11c38a58e97ecb7ef9e979e80f
VishnuVardhanYarra <161280821+VishnuVardhanYarra@users.noreply.github.com>
2025-06-28 22:08:17 +0530

    Delete Executable Files/check.py

--- Executable Files/check.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv("Data/liver_data.csv")
-
-# replace with the exact target column name you used
-TARGET_COL = "Predicted Value(Out Come-Patient suffering from liver  cirrosis or not)"
-
-print("\nUnique raw labels and counts:")
-print(df[TARGET_COL].value_counts(dropna=False))
